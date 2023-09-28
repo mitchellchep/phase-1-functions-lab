d80768fb210b03f30cb22ad2a9803e712dbc577c
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-28 19:12:02 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+  const headquartersBlock = 42;
+  return Math.abs(pickupBlock - headquartersBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock){
+  const blocks = distanceFromHqInBlocks(pickupBlock);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(startBlock, destinationBlock){
+  return Math.abs(destinationBlock - startBlock)*264;
+}
+
+function calculatesFarePrice(startBlock, destinationBlock){
+  const distance = distanceTravelledInFeet(startBlock, destinationBlock);
+   
+      if (distance <= 400){
+      return 0;
+      } else if (distance <= 2000){
+        return (distance - 400)*0.02;
+      } else if (distance <= 2500){
+        return 25;
+      }else {
+        return 'cannot travel that far';
+      }
+
+}
